d07ce72d3b770a9895cd656755019a0f5c23f2db
Florent Tariolle <florent.tariolle@interdigital.com>
2026-06-03 17:30:01 +0200

    style: use animated theme toggle

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -32,10 +32,14 @@ let setTheme = (theme) => {
 
   if (use_theme === "dark") {
     $("html").attr("data-theme", "dark");
-    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
+    $("#theme-toggle")
+      .attr("aria-pressed", "true")
+      .attr("aria-label", "Switch to light theme");
   } else if (use_theme === "light") {
     $("html").removeAttr("data-theme");
-    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
+    $("#theme-toggle")
+      .attr("aria-pressed", "false")
+      .attr("aria-label", "Switch to dark theme");
   }
 };
 
